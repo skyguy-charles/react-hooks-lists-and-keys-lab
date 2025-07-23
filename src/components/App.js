76e7c64ff645d7ea5ca4f0ff25fed82c7e172507
charles <cyphersmae@gmail.com>
2025-07-23 15:06:33 +0300

    complete react-hooks-list-keys

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,47 @@
+// App.js
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-import ProjectList from "./ProjectList";
-import user from "../data/user";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
+import ProjectList from "./components/ProjectList";
 
+const user = {
+  name: "Charles Mwangi",
+  city: "Nairobi",
+  bio: "Full-stack developer passionate about building financial tools.",
+  links: {
+    github: "https://github.com/charlesmwangi",
+    linkedin: "https://linkedin.com/in/charlesmwangi"
+  }
+};
+
+const projects = [
+  {
+    id: 1,
+    name: "Portfolio Website",
+    about: "A personal site to showcase my work",
+    technologies: ["React", "CSS", "JavaScript"]
+  },
+  {
+    id: 2,
+    name: "Budget Tracker",
+    about: "Track expenses and manage savings goals",
+    technologies: ["React", "Node.js", "Express"]
+  },
+  {
+    id: 3,
+    name: "Weather App",
+    about: "Get real-time weather updates",
+    technologies: ["HTML", "CSS", "API"]
+  }
+];
 function App() {
   return (
     <div>
       <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <Home name={user.name} city={user.city} />
+      <About bio={user.bio} github={user.links.github} linkedin={user.links.linkedin} />
+      <ProjectList projects={projects} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import ProjectItem from "./components/ProjectItem";
+import ProjectList from "./components/ProjectList";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+<StrictMode>
+<ProjectItem/>
+<ProjectList></ProjectList>
+</StrictMode>
